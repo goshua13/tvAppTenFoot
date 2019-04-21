50b57df680f4e25ab97375d14c7daba21e823a9b
Joshua Aragon <joshuaaragon11@gmail.com>
2019-04-21 15:58:52 -0600

    last commit kinda....

--- src/components/content-components/company.js
+++ src/components/content-components/company.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { View, Text} from 'react-native';
 import { connect } from 'react-redux';
 
+import styles from '../../styles';
+
 class Company extends Component {
     renderCompany() {
         const { users, menuId } = this.props;
